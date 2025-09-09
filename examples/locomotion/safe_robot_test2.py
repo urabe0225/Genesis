3e30a76f134b0bc833696b8e5c252ad9ff641ec3
Kazuki Urabe <urabe.kazuki@tis.co.jp>
2025-09-09 17:30:43 +0900

    safe_robot_test2

--- examples/locomotion/safe_robot_test2.py
+++ examples/locomotion/safe_robot_test2.py
@@ -104,20 +104,21 @@ class SafeGo2Controller:
         # print("IMU state: ", msg.imu_state)
         # print("Battery state: voltage: ", msg.power_v, "current: ", msg.power_a)
 
-    def input_low_cmd(self, q, dq, kp, kd, tau):
-        for motor_id in range(12):
-            self.low_cmd.motor_cmd[motor_id].q = q
-            self.low_cmd.motor_cmd[motor_id].dq = dq
-            self.low_cmd.motor_cmd[motor_id].kp = kp
-            self.low_cmd.motor_cmd[motor_id].kd = kd
-            self.low_cmd.motor_cmd[motor_id].tau = tau
+    def input_low_cmd(self, motor_id, q, dq, kp, kd, tau):
+        self.low_cmd.motor_cmd[motor_id].q = q
+        self.low_cmd.motor_cmd[motor_id].dq = dq
+        self.low_cmd.motor_cmd[motor_id].kp = kp
+        self.low_cmd.motor_cmd[motor_id].kd = kd
+        self.low_cmd.motor_cmd[motor_id].tau = tau
 
     def LowCmdWrite(self):
 
         self.percents[0] += 1.0 / self.durations[0]
         self.percents[0] = min(self.percents[0], 1)
         if self.percents[0] < 1:
-            self.input_low_cmd((1 - self.percents[0]) * self.startPos[i] + self.percents[0] * self._targetPos_1[i],
+            for i in range(12):
+                self.input_low_cmd(i,
+                                   (1 - self.percents[0]) * self.startPos[i] + self.percents[0] * self._targetPos_1[i],
                                    0,
                                    self.Kp,
                                    self.Kd,
@@ -126,7 +127,8 @@ class SafeGo2Controller:
         if (self.percents[0] == 1) and (self.percents[1] <= 1):
             self.percents[1] += 1.0 / self.durations[1]
             self.percents[1] = min(self.percents[1], 1)
-            self.input_low_cmd(
+            for i in range(12):
+                self.input_low_cmd(i,
                                    (1 - self.percents[1]) * self._targetPos_1[i] + self.percents[1] * self._targetPos_2[i],
                                    0,
                                    self.Kp,
@@ -136,7 +138,8 @@ class SafeGo2Controller:
         if (self.percents[0] == 1) and (self.percents[1] == 1) and (self.percents[2] < 1):
             self.percents[2] += 1.0 / self.durations[2]
             self.percents[2] = min(self.percents[2], 1)
-            self.input_low_cmd(
+            for i in range(12):
+                self.input_low_cmd(i,
                                    self._targetPos_2[i],
                                    0,
                                    self.Kp,
@@ -146,7 +149,8 @@ class SafeGo2Controller:
         if (self.percents[0] == 1) and (self.percents[1] == 1) and (self.percents[2] == 1) and (self.percents[3] <= 1):
             self.percents[3] += 1.0 / self.durations[3]
             self.percents[3] = min(self.percents[3], 1)
-            self.input_low_cmd(
+            for i in range(12):
+                self.input_low_cmd(i,
                                    (1 - self.percents[3]) * self._targetPos_2[i] + self.percents[3] * self._targetPos_3[i],
                                    0,
                                    self.Kp,
