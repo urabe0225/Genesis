59c669efaee5b52a9fc93210ed30cf1bb880407b
Kazuki Urabe <urabe.kazuki@tis.co.jp>
2025-09-11 14:44:38 +0900

    safe_robot_test2

--- examples/locomotion/safe_robot_test2.py
+++ examples/locomotion/safe_robot_test2.py
@@ -1,52 +1,39 @@
 import time
 import sys
-try:
-    import onnx
-    import onnxruntime as ort
-    print("✓ ONNX and ONNX Runtime are available")
-    ONNX_AVAILABLE = True
-except ImportError:
-    print("✗ ONNX or ONNX Runtime not available. Please install via 'pip install onnx onnxruntime'")
-    ONNX_AVAILABLE = False
-    from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelPublisher, ChannelFactoryInitialize
-    from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowState_
-    from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
-    from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_, LowCmd_
-    from unitree_sdk2py.utils.crc import CRC
-    from unitree_sdk2py.utils.thread import RecurrentThread
-    from unitree_sdk2py.comm.motion_switcher.motion_switcher_client import MotionSwitcherClient
-    from unitree_sdk2py.go2.sport.sport_client import SportClient
 
-class Go2Controller:
-    def __init__(self, onnx_model_path=None):
-        if UNITREE_SDK_AVAILABLE:
-            self.crc = CRC()
-        else:
-            self.crc = None
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
+from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowState_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_
+from unitree_sdk2py.utils.crc import CRC
+from unitree_sdk2py.utils.thread import RecurrentThread
+import unitree_legged_const as go2
+from unitree_sdk2py.comm.motion_switcher.motion_switcher_client import MotionSwitcherClient
+from unitree_sdk2py.go2.sport.sport_client import SportClient
 
-        if onnx_model_path and ONNX_AVAILABLE:
-            try:
-                self.session = ort.InferenceSession(onnx_model_path)
-                print(f"✓ ONNX model loaded: {onnx_model_path}")
-            except Exception as e:
-                print(f"Error loading ONNX model: {e}")
-        elif onnx_model_path:
-            print("✗ ONNX model specified but onnxruntime not available")
-
+class Custom:
+    def __init__(self):
         self.Kp = 60.0
         self.Kd = 5.0
+        self.time_consume = 0
+        self.rate_count = 0
+        self.sin_count = 0
+        self.motiontime = 0
+        self.dt = 0.002  # 0.001~0.01
 
         self.low_cmd = unitree_go_msg_dds__LowCmd_()  
         self.low_state = None  
 
-        self.startPos = [0.0] * 12
-        self.standing_pose = [0.0, 1.36, -2.65, 0.0, 1.36, -2.65,
+        self._targetPos_1 = [0.0, 1.36, -2.65, 0.0, 1.36, -2.65,
                              -0.2, 1.36, -2.65, 0.2, 1.36, -2.65]
-        self.stand_pose = [0.0, 0.67, -1.3, 0.0, 0.67, -1.3,
+        self._targetPos_2 = [0.0, 0.67, -1.3, 0.0, 0.67, -1.3,
                              0.0, 0.67, -1.3, 0.0, 0.67, -1.3]
-        self.rest_pose = [-0.35, 1.36, -2.65, 0.35, 1.36, -2.65,
+        self._targetPos_3 = [-0.35, 1.36, -2.65, 0.35, 1.36, -2.65,
                              -0.5, 1.36, -2.65, 0.5, 1.36, -2.65]
 
+        self.startPos = [0.0] * 12
         self.duration_1 = 500
         self.duration_2 = 500
         self.duration_3 = 1000
@@ -57,13 +44,15 @@ class Go2Controller:
         self.percent_4 = 0
 
         self.firstRun = True
+        self.done = False
 
         # thread handling
         self.lowCmdWriteThreadPtr = None
 
+        self.crc = CRC()
+
     # Public methods
     def Init(self):
-        
         self.InitLowCmd()
 
         # create publisher #
@@ -103,9 +92,9 @@ class Go2Controller:
         self.low_cmd.gpio = 0
         for i in range(20):
             self.low_cmd.motor_cmd[i].mode = 0x01  # (PMSM) mode
-            self.low_cmd.motor_cmd[i].q= 2.146e9
+            self.low_cmd.motor_cmd[i].q= go2.PosStopF
             self.low_cmd.motor_cmd[i].kp = 0
-            self.low_cmd.motor_cmd[i].dq = 16000.0
+            self.low_cmd.motor_cmd[i].dq = go2.VelStopF
             self.low_cmd.motor_cmd[i].kd = 0
             self.low_cmd.motor_cmd[i].tau = 0
 
@@ -126,7 +115,7 @@ class Go2Controller:
         self.percent_1 = min(self.percent_1, 1)
         if self.percent_1 < 1:
             for i in range(12):
-                self.low_cmd.motor_cmd[i].q = (1 - self.percent_1) * self.startPos[i] + self.percent_1 * self.standing_pose[i]
+                self.low_cmd.motor_cmd[i].q = (1 - self.percent_1) * self.startPos[i] + self.percent_1 * self._targetPos_1[i]
                 self.low_cmd.motor_cmd[i].dq = 0
                 self.low_cmd.motor_cmd[i].kp = self.Kp
                 self.low_cmd.motor_cmd[i].kd = self.Kd
@@ -136,7 +125,7 @@ class Go2Controller:
             self.percent_2 += 1.0 / self.duration_2
             self.percent_2 = min(self.percent_2, 1)
             for i in range(12):
-                self.low_cmd.motor_cmd[i].q = (1 - self.percent_2) * self.standing_pose[i] + self.percent_2 * self.stand_pose[i]
+                self.low_cmd.motor_cmd[i].q = (1 - self.percent_2) * self._targetPos_1[i] + self.percent_2 * self._targetPos_2[i]
                 self.low_cmd.motor_cmd[i].dq = 0
                 self.low_cmd.motor_cmd[i].kp = self.Kp
                 self.low_cmd.motor_cmd[i].kd = self.Kd
@@ -146,7 +135,7 @@ class Go2Controller:
             self.percent_3 += 1.0 / self.duration_3
             self.percent_3 = min(self.percent_3, 1)
             for i in range(12):
-                self.low_cmd.motor_cmd[i].q = self.stand_pose[i] 
+                self.low_cmd.motor_cmd[i].q = self._targetPos_2[i] 
                 self.low_cmd.motor_cmd[i].dq = 0
                 self.low_cmd.motor_cmd[i].kp = self.Kp
                 self.low_cmd.motor_cmd[i].kd = self.Kd
@@ -156,7 +145,7 @@ class Go2Controller:
             self.percent_4 += 1.0 / self.duration_4
             self.percent_4 = min(self.percent_4, 1)
             for i in range(12):
-                self.low_cmd.motor_cmd[i].q = (1 - self.percent_4) * self.stand_pose[i] + self.percent_4 * self.rest_pose[i]
+                self.low_cmd.motor_cmd[i].q = (1 - self.percent_4) * self._targetPos_2[i] + self.percent_4 * self._targetPos_3[i]
                 self.low_cmd.motor_cmd[i].dq = 0
                 self.low_cmd.motor_cmd[i].kp = self.Kp
                 self.low_cmd.motor_cmd[i].kd = self.Kd
@@ -165,36 +154,23 @@ class Go2Controller:
         self.low_cmd.crc = self.crc.Crc(self.low_cmd)
         self.lowcmd_publisher.Write(self.low_cmd)
 
-def main():
-    print("=== Go2 Safe Testing Protocol ===")
+if __name__ == '__main__':
+
     print("WARNING: Please ensure there are no obstacles around the robot while running this example.")
     input("Press Enter to continue...")
 
-    # Model path (optional)
-    model_path = "logs/go2-walking/policy_100.onnx"
+    if len(sys.argv)>1:
+        ChannelFactoryInitialize(0, sys.argv[1])
+    else:
+        ChannelFactoryInitialize(0)
 
-    try:
-        if len(sys.argv)>1:
-            ChannelFactoryInitialize(0, sys.argv[1])
-        else:
-            ChannelFactoryInitialize(0)
-        custom = Go2Controller()
-        custom.Init()
-        custom.Start()
-        while True:        
-            if custom.percent_4 == 1.0: 
-                time.sleep(1)
-                print("Done!")
-                sys.exit(-1)     
-            time.sleep(1)
-    except Exception as e:
-        print(f"\n✗ Error during testing: {e}")
-        import traceback
-        traceback.print_exc()
-    except KeyboardInterrupt:
-        print("\n⚠ Testing interrupted by user")
+    custom = Custom()
+    custom.Init()
+    custom.Start()
 
-
-
-if __name__ == '__main__':
-    main()
+    while True:        
+        if custom.percent_4 == 1.0: 
+           time.sleep(1)
+           print("Done!")
+           sys.exit(-1)     
+        time.sleep(1)
