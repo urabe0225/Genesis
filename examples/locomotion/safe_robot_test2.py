d3a2fb3a447227254a0c14dfe40dbd21d5fcc956
Kazuki Urabe <urabe.kazuki@tis.co.jp>
2025-09-11 11:16:13 +0900

    safe_robot_test2

--- examples/locomotion/safe_robot_test2.py
+++ examples/locomotion/safe_robot_test2.py
@@ -2,109 +2,101 @@ import time
 import sys
 
 from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
-from unitree_sdk2py.core.channel import ChannelSubscriber
+from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
 from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
 from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowState_
 from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_
 from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_
 from unitree_sdk2py.utils.crc import CRC
 from unitree_sdk2py.utils.thread import RecurrentThread
+import unitree_legged_const as go2
 from unitree_sdk2py.comm.motion_switcher.motion_switcher_client import MotionSwitcherClient
 from unitree_sdk2py.go2.sport.sport_client import SportClient
 
-class SafeGo2Controller:
+class Custom:
     def __init__(self):
         self.Kp = 60.0
         self.Kd = 5.0
-        self.crc = CRC()
-        
-        self.low_cmd = unitree_go_msg_dds__LowCmd_()
+        self.time_consume = 0
+        self.rate_count = 0
+        self.sin_count = 0
+        self.motiontime = 0
+        self.dt = 0.002  # 0.001~0.01
+
+        self.low_cmd = unitree_go_msg_dds__LowCmd_()  
         self.low_state = None  
 
-        self.startPos = [0.0] * 12
-        self.standing_pose = [0.0, 1.36, -2.65, 0.0, 1.36, -2.65,
+        self._targetPos_1 = [0.0, 1.36, -2.65, 0.0, 1.36, -2.65,
                              -0.2, 1.36, -2.65, 0.2, 1.36, -2.65]
-        self.stand_pose = [0.0, 0.67, -1.3] * 4  # 4脚 × 3関節
-        self.rest_pose = [-0.35, 1.36, -2.65, 0.35, 1.36, -2.65,
+        self._targetPos_2 = [0.0, 0.67, -1.3, 0.0, 0.67, -1.3,
+                             0.0, 0.67, -1.3, 0.0, 0.67, -1.3]
+        self._targetPos_3 = [-0.35, 1.36, -2.65, 0.35, 1.36, -2.65,
                              -0.5, 1.36, -2.65, 0.5, 1.36, -2.65]
 
-        self.durations = [500, 500, 1000, 900]  # ms
-        self.percents = [0.0, 0.0, 0.0]#[1.0, 0.0, 1.0, 0.0]
-        self.process = 0.0
+        self.startPos = [0.0] * 12
+        self.duration_1 = 500
+        self.duration_2 = 500
+        self.duration_3 = 1000
+        self.duration_4 = 900
+        self.percent_1 = 0
+        self.percent_2 = 0
+        self.percent_3 = 0
+        self.percent_4 = 0
 
-        self.low_level = False
+        self.firstRun = True
+        self.done = False
+
         # thread handling
         self.lowCmdWriteThreadPtr = None
 
-    def init_command(self):
-        cmd = unitree_go_msg_dds__LowCmd_()
-        cmd.head[0]=0xFE
-        cmd.head[1]=0xEF
-        cmd.level_flag = 0xFF
-        cmd.gpio = 0
-        for i in range(20):
-            cmd.motor_cmd[i].mode = 0x01  # PMSM mode
-            cmd.motor_cmd[i].q= 2.146e9
-            cmd.motor_cmd[i].kp = 0
-            cmd.motor_cmd[i].dq = 16000.0
-            cmd.motor_cmd[i].kd = 0
-            cmd.motor_cmd[i].tau = 0
-        return cmd
+        self.crc = CRC()
 
-    def mode_release(self):
-        max_attempts = 5
-        attempt = 0
-        msc = MotionSwitcherClient()
-        sc = SportClient()  
-        while attempt < max_attempts:
-            status, result = msc.CheckMode()
-
-            if not result or not result.get('name'):
-                print("✓ All modes released successfully")
-                return True
-                
-            mode_name = result['name']
-            print(f"Releasing mode: {mode_name} (attempt {attempt + 1})")
-            
-            sc.StandDown()
-            msc.ReleaseMode()
-            time.sleep(1)
-            attempt += 1
-        
-        print("⚠ Warning: Could not release all modes")
-        return False
-
-    def Get_position(self):
-        for i in range(12):
-            self.startPos[i] = self.low_state.motor_state[i].q
-
+    # Public methods
     def Init(self):
-        self.low_cmd = self.init_command()
+        self.InitLowCmd()
 
-        # create publisher
+        # create publisher #
         self.lowcmd_publisher = ChannelPublisher("rt/lowcmd", LowCmd_)
         self.lowcmd_publisher.Init()
 
-        # create subscriber
+        # create subscriber # 
         self.lowstate_subscriber = ChannelSubscriber("rt/lowstate", LowState_)
         self.lowstate_subscriber.Init(self.LowStateMessageHandler, 10)
 
-        self.mode_release()
-        self.Get_position()
+        self.sc = SportClient()  
+        self.sc.SetTimeout(5.0)
+        self.sc.Init()
+
+        self.msc = MotionSwitcherClient()
+        self.msc.SetTimeout(5.0)
+        self.msc.Init()
+
+        status, result = self.msc.CheckMode()
+        while result['name']:
+            self.sc.StandDown()
+            self.msc.ReleaseMode()
+            status, result = self.msc.CheckMode()
+            time.sleep(1)
 
     def Start(self):
         self.lowCmdWriteThreadPtr = RecurrentThread(
             interval=0.002, target=self.LowCmdWrite, name="writebasiccmd"
         )
         self.lowCmdWriteThreadPtr.Start()
-        while True:        
-            if self.percents[2] == 1.0: 
-                time.sleep(1)
-                print("Done!")
-                sys.exit(-1)     
-            time.sleep(1)
 
-
+    # Private methods
+    def InitLowCmd(self):
+        self.low_cmd.head[0]=0xFE
+        self.low_cmd.head[1]=0xEF
+        self.low_cmd.level_flag = 0xFF
+        self.low_cmd.gpio = 0
+        for i in range(20):
+            self.low_cmd.motor_cmd[i].mode = 0x01  # (PMSM) mode
+            self.low_cmd.motor_cmd[i].q= go2.PosStopF
+            self.low_cmd.motor_cmd[i].kp = 0
+            self.low_cmd.motor_cmd[i].dq = go2.VelStopF
+            self.low_cmd.motor_cmd[i].kd = 0
+            self.low_cmd.motor_cmd[i].tau = 0
 
     def LowStateMessageHandler(self, msg: LowState_):
         self.low_state = msg
@@ -112,81 +104,73 @@ class SafeGo2Controller:
         # print("IMU state: ", msg.imu_state)
         # print("Battery state: voltage: ", msg.power_v, "current: ", msg.power_a)
 
-    def input_low_cmd(self, motor_id, q, dq, tau):
-        self.low_cmd.motor_cmd[motor_id].q = q
-        self.low_cmd.motor_cmd[motor_id].dq = dq
-        self.low_cmd.motor_cmd[motor_id].kp = self.Kp
-        self.low_cmd.motor_cmd[motor_id].kd = self.Kd
-        self.low_cmd.motor_cmd[motor_id].tau = tau
-
-
-    def clock(self,i):
-        self.percents[i] += 1.0 / self.durations[i]
-        self.percents[i] = min(self.percents[i], 1)
-
     def LowCmdWrite(self):
-        self.clock(0)
-        if self.percents[0] < 1:
+
+        if self.firstRun:
             for i in range(12):
-                self.input_low_cmd(i,
-                                   (1 - self.percents[0]) * self.startPos[i] + self.percents[0] * self.standing_pose[i],
-                                   0,
-                                   0)
-        
-        if (self.percents[0] == 1) and (self.percents[1] <= 1):
-            self.clock(1)
+                self.startPos[i] = self.low_state.motor_state[i].q
+            self.firstRun = False
+
+        self.percent_1 += 1.0 / self.duration_1
+        self.percent_1 = min(self.percent_1, 1)
+        if self.percent_1 < 1:
             for i in range(12):
-                self.input_low_cmd(i,
-                                   (1 - self.percents[1]) * self.standing_pose[i] + self.percents[1] * self.stand_pose[i],
-                                   0,
-                                   0)
-        """
-        if (self.percents[0] == 1) and (self.percents[1] == 1) and (self.percents[2] < 1):
-            self.clock(2)
+                self.low_cmd.motor_cmd[i].q = (1 - self.percent_1) * self.startPos[i] + self.percent_1 * self._targetPos_1[i]
+                self.low_cmd.motor_cmd[i].dq = 0
+                self.low_cmd.motor_cmd[i].kp = self.Kp
+                self.low_cmd.motor_cmd[i].kd = self.Kd
+                self.low_cmd.motor_cmd[i].tau = 0
+
+        if (self.percent_1 == 1) and (self.percent_2 <= 1):
+            self.percent_2 += 1.0 / self.duration_2
+            self.percent_2 = min(self.percent_2, 1)
             for i in range(12):
-                self.input_low_cmd(i,
-                                   self.stand_pose[i],
-                                   0,
-                                   0)
-        """
-        if (self.percents[0] == 1) and (self.percents[1] == 1) and (self.percents[2] <= 1):
-            self.clock(2)
+                self.low_cmd.motor_cmd[i].q = (1 - self.percent_2) * self._targetPos_1[i] + self.percent_2 * self._targetPos_2[i]
+                self.low_cmd.motor_cmd[i].dq = 0
+                self.low_cmd.motor_cmd[i].kp = self.Kp
+                self.low_cmd.motor_cmd[i].kd = self.Kd
+                self.low_cmd.motor_cmd[i].tau = 0
+
+        if (self.percent_1 == 1) and (self.percent_2 == 1) and (self.percent_3 < 1):
+            self.percent_3 += 1.0 / self.duration_3
+            self.percent_3 = min(self.percent_3, 1)
             for i in range(12):
-                self.input_low_cmd(i,
-                                   (1 - self.percents[2]) * self.stand_pose[i] + self.percents[2] * self.rest_pose[i],
-                                   0,
-                                   0)
+                self.low_cmd.motor_cmd[i].q = self._targetPos_2[i] 
+                self.low_cmd.motor_cmd[i].dq = 0
+                self.low_cmd.motor_cmd[i].kp = self.Kp
+                self.low_cmd.motor_cmd[i].kd = self.Kd
+                self.low_cmd.motor_cmd[i].tau = 0
+
+        if (self.percent_1 == 1) and (self.percent_2 == 1) and (self.percent_3 == 1) and (self.percent_4 <= 1):
+            self.percent_4 += 1.0 / self.duration_4
+            self.percent_4 = min(self.percent_4, 1)
+            for i in range(12):
+                self.low_cmd.motor_cmd[i].q = (1 - self.percent_4) * self._targetPos_2[i] + self.percent_4 * self._targetPos_3[i]
+                self.low_cmd.motor_cmd[i].dq = 0
+                self.low_cmd.motor_cmd[i].kp = self.Kp
+                self.low_cmd.motor_cmd[i].kd = self.Kd
+                self.low_cmd.motor_cmd[i].tau = 0
 
         self.low_cmd.crc = self.crc.Crc(self.low_cmd)
         self.lowcmd_publisher.Write(self.low_cmd)
 
-def main():
-    print("=== Go2 Safe Testing Protocol ===")
+if __name__ == '__main__':
+
     print("WARNING: Please ensure there are no obstacles around the robot while running this example.")
     input("Press Enter to continue...")
 
-    # Model path (optional)
-    model_path = "logs/go2-walking/policy_100.onnx"
+    if len(sys.argv)>1:
+        ChannelFactoryInitialize(0, sys.argv[1])
+    else:
+        ChannelFactoryInitialize(0)
 
-    try:
-        if len(sys.argv)>1:
-            ChannelFactoryInitialize(0, sys.argv[1])
-        else:
-            ChannelFactoryInitialize(0)
-        controller = SafeGo2Controller()
-        input("\nPress Enter to start Test 1 (Standing pose)...")
-        controller.Init()
-        controller.Start()
+    custom = Custom()
+    custom.Init()
+    custom.Start()
 
-    except Exception as e:
-        print(f"\n✗ Error during testing: {e}")
-        import traceback
-        traceback.print_exc()
-    except KeyboardInterrupt:
-        print("\n⚠ Testing interrupted by user")
-
-if __name__ == '__main__':
-    main()
-
-    
-
+    while True:        
+        if custom.percent_4 == 1.0: 
+           time.sleep(1)
+           print("Done!")
+           sys.exit(-1)     
+        time.sleep(1)
